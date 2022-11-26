6765059ade7a5c0cdbf88803209011f142984689
Natali Guimarães Paulino <85068406+nataligp@users.noreply.github.com>
2022-11-26 20:21:28 -0300

    finalizado

--- js/fonte.js
+++ js/fonte.js
@@ -1,14 +1,24 @@
 $(document).ready(function(){
-    let fonte = 14
+    let fonte = 16
     $('#aumenta').click(function(){
-        if(fonte<22){
+        if(fonte<24){
             //fonte++
             $('body').css({'font-size': ++fonte+'px'})
+            $('#cata a').css({'font-size': ++fonte+'px'})
+            $('#btncat').css({'font-size': ++fonte+'px'})
+            $('#produtos p').css({'font-size': ++fonte+'px'})
+            $('#produtos a').css({'font-size': ++fonte+'px'})
+            $('#btnformreg').css({'font-size': ++fonte+'px'})
         }
     })
     $('#diminui').click(function(){
         if(fonte>14){
             $('body').css({'font-size': --fonte+'px'})
+            $('#cata a').css({'font-size': --fonte+'px'})
+            $('#btncat').css({'font-size': --fonte+'px'})
+            $('#produtos p').css({'font-size': --fonte+'px'})
+            $('#produtos a').css({'font-size': --fonte+'px'})
+            $('#btnformreg').css({'font-size': --fonte+'px'})
         }
     })
 })
---
+++ js/mascara.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $('#tel_cad').mask('(99) 99999-9999')
+})
