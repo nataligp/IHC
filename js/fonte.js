3770d8511ccf0f22efe751170b2689177928ceef
Natali Guimarães Paulino <85068406+nataligp@users.noreply.github.com>
2022-11-28 11:59:05 -0300

    correções gerais

--- js/fonte.js
+++ js/fonte.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     let fonte = 16
     $('#aumenta').click(function(){
-        if(fonte<24){
+        if(fonte<30){
             //fonte++
             $('body').css({'font-size': ++fonte+'px'})
             $('#cata a').css({'font-size': ++fonte+'px'})
@@ -9,16 +9,27 @@ $(document).ready(function(){
             $('#produtos p').css({'font-size': ++fonte+'px'})
             $('#produtos a').css({'font-size': ++fonte+'px'})
             $('#btnformreg').css({'font-size': ++fonte+'px'})
+            $('#nome_cad').css({'font-size': ++fonte+'px'})
+            $('#email_cad').css({'font-size': ++fonte+'px'})
+            $('#tel_cad').css({'font-size': ++fonte+'px'})
+            $('#mensagem').css({'font-size': ++fonte+'px'})
+            $('#desc').css({'font-size': ++fonte+'px'})
+            $('#descinput').css({'font-size': ++fonte+'px'})
         }
     })
     $('#diminui').click(function(){
-        if(fonte>14){
+        if(fonte>10){
             $('body').css({'font-size': --fonte+'px'})
             $('#cata a').css({'font-size': --fonte+'px'})
             $('#btncat').css({'font-size': --fonte+'px'})
             $('#produtos p').css({'font-size': --fonte+'px'})
             $('#produtos a').css({'font-size': --fonte+'px'})
             $('#btnformreg').css({'font-size': --fonte+'px'})
+            $('#nome_cad').css({'font-size': --fonte+'px'})
+            $('#email_cad').css({'font-size': --fonte+'px'})
+            $('#tel_cad').css({'font-size': --fonte+'px'})
+            $('#mensagem').css({'font-size': --fonte+'px'})
+            $('#desc').css({'font-size': --fonte+'px'})
         }
     })
 })
