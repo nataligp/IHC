d7bb947a11f908cbe40d1ac63480c6c763f36057
Natali Guimarães Paulino <85068406+nataligp@users.noreply.github.com>
2022-11-29 15:07:53 -0300

    aumentar/diminuir fonte

--- js/fonte.js
+++ js/fonte.js
@@ -1,35 +1,19 @@
 $(document).ready(function(){
     let fonte = 16
     $('#aumenta').click(function(){
-        if(fonte<30){
-            //fonte++
+        if(fonte<=24){
             $('body').css({'font-size': ++fonte+'px'})
-            $('#cata a').css({'font-size': ++fonte+'px'})
-            $('#btncat').css({'font-size': ++fonte+'px'})
-            $('#produtos p').css({'font-size': ++fonte+'px'})
-            $('#produtos a').css({'font-size': ++fonte+'px'})
-            $('#btnformreg').css({'font-size': ++fonte+'px'})
-            $('#nome_cad').css({'font-size': ++fonte+'px'})
-            $('#email_cad').css({'font-size': ++fonte+'px'})
-            $('#tel_cad').css({'font-size': ++fonte+'px'})
-            $('#mensagem').css({'font-size': ++fonte+'px'})
-            $('#desc').css({'font-size': ++fonte+'px'})
-            $('#descinput').css({'font-size': ++fonte+'px'})
+            $('.formdescricao label, input, button').css({'font-size': ++fonte+'px'})
+            $('.produto-single a').css({'font-size': ++fonte+'px'})
+            $('.produto-single p').css({'font-size': ++fonte+'px'})
         }
     })
     $('#diminui').click(function(){
-        if(fonte>10){
+        if(fonte>=12){
             $('body').css({'font-size': --fonte+'px'})
-            $('#cata a').css({'font-size': --fonte+'px'})
-            $('#btncat').css({'font-size': --fonte+'px'})
-            $('#produtos p').css({'font-size': --fonte+'px'})
-            $('#produtos a').css({'font-size': --fonte+'px'})
-            $('#btnformreg').css({'font-size': --fonte+'px'})
-            $('#nome_cad').css({'font-size': --fonte+'px'})
-            $('#email_cad').css({'font-size': --fonte+'px'})
-            $('#tel_cad').css({'font-size': --fonte+'px'})
-            $('#mensagem').css({'font-size': --fonte+'px'})
-            $('#desc').css({'font-size': --fonte+'px'})
+            $('.formdescricao label, input, button').css({'font-size': --fonte+'px'})
+            $('.produto-single a').css({'font-size': --fonte+'px'})
+            $('.produto-single p').css({'font-size': --fonte+'px'})
         }
     })
 })
